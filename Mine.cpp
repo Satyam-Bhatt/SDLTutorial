6d3d1bc67d7a7277037925ba21e8f87830b2b9f9
Satyam-Bhatt <satyambhatt20@gmail.com>
2024-11-01 12:38:41 +0530

    Loaded an image and created a class

---
+++ Mine.cpp
@@ -0,0 +1,100 @@
+#include <SDL.h>
+#include <stdio.h>
+
+SDL_Window* window = NULL;
+SDL_Surface* screenSurface = NULL;
+//Image
+SDL_Surface* image = NULL;
+
+class MineFirstClass {
+	//Screen Dimensions
+	const int S_Widht = 800;
+	const int S_Height = 600;
+
+//public:bool init_Mine();
+//public:bool loadMedia_Mine();
+//public:void close_Mine();
+	  //Initialzie
+public:bool init_Mine()
+{
+	bool success = false;
+	if (SDL_Init((SDL_INIT_VIDEO)) < 0)
+	{
+		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+		return false;
+	}
+	else
+	{
+		window = SDL_CreateWindow("Rendering Image", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, S_Widht, S_Height, SDL_WINDOW_MAXIMIZED);
+		if (window == NULL)
+		{
+			printf("Window could not come thright %s", SDL_GetError());
+			return false;
+		}
+		else
+		{
+			screenSurface = SDL_GetWindowSurface(window);
+			//SDL_UpdateWindowSurface(window);
+			success = true;
+		}
+	}
+	return success;
+}
+
+public:bool loadMedia_Mine()
+{
+	bool success = true;
+
+	image = SDL_LoadBMP("02_getting_an_image_on_the_screen/hello_world.bmp");
+	if (image == NULL)
+	{
+		printf("error %s", SDL_GetError());
+		success = false;
+	}
+
+	return success;
+}
+
+public:void close_Mine()
+{
+	SDL_FreeSurface(image);
+	image = NULL;
+	SDL_DestroyWindow(window);
+	window = NULL;
+	SDL_Quit();
+}
+};
+
+
+int main( int argc, char* args[] )
+{
+	MineFirstClass firstClass;
+
+	if( !firstClass.init_Mine() )
+	{
+		printf( "Failed to initialize!\n" );
+	}
+	else
+	{
+		if( !firstClass.loadMedia_Mine() )
+		{
+			printf( "Failed to load media!\n" );
+		}
+		else
+		{
+			//Apply the image
+			SDL_BlitSurface( image, NULL, screenSurface, NULL );
+			
+			//Update the surface
+			SDL_UpdateWindowSurface( window );
+
+            //Hack to get window to stay up
+            SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
+		}
+	}
+
+	//Free resources and close SDL
+	firstClass.close_Mine();
+
+	return 0;
+}
