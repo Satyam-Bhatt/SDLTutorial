0ef0ccb0558050d312e015c2a8b4920a17ca69f8
Satyam-Bhatt <satyambhatt20@gmail.com>
2024-12-08 18:12:54 +0530

    Frame Capping removed from one main project

--- CapScreen_Main.cpp
+++ CapScreen_Main.cpp
@@ -1,297 +1,297 @@
-// Manually Capping Screen Time
-
-#include <SDL.h>
-#include <stdio.h>
-#include <string>
-#include <SDL_ttf.h>
-#include <sstream>
-
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
-const int SCREEN_FPS = 60;
-const int SCREEN_TICK_PER_FRAME = 1000 / SCREEN_FPS;
-
-class LTexture
-{
-public:
-	//Initializes variables
-	LTexture();
-
-	//Deallocates memory
-	~LTexture();
-
-	//Creates image from font string
-	bool loadFromRenderedText(std::string textureText, SDL_Color textColor);
-
-	//Deallocates texture
-	void free();
-
-	void render(int x, int y, SDL_Rect* clip = NULL, double angle = 0, SDL_Point* center = NULL, SDL_RendererFlip flip = SDL_FLIP_NONE);
-
-private:
-	//The actual hardware texture
-	SDL_Texture* mTexture;
-	int mWidth;
-	int mHeight;
-};
-
-LTexture::LTexture()
-{
-	//Initialize
-	mTexture = NULL;
-	mWidth = 0;
-	mHeight = 0;
-}
-
-LTexture::~LTexture()
-{
-	//Deallocate
-	free();
-}
-
-void LTexture::free()
-{
-	if (mTexture != NULL)
-	{
-		SDL_DestroyTexture(mTexture);
-		mTexture = NULL;
-		mWidth = 0;
-		mHeight = 0;
-	}
-}
-
-SDL_Window* window = NULL;
-SDL_Renderer* renderer = NULL;
-
-TTF_Font* fontFace = NULL;
-LTexture gFPSTextTexture;
-
-bool LTexture::loadFromRenderedText(std::string textureText, SDL_Color textColor)
-{
-	free();
-
-	SDL_Surface* textSurface = TTF_RenderText_Solid(fontFace, textureText.c_str(), textColor);
-	if (textSurface != NULL)
-	{
-		mTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
-		if (mTexture == NULL)
-		{
-			printf("Unable to create texture from rendered text! SDL Error %s\n", SDL_GetError());
-		}
-		else
-		{
-			mWidth = textSurface->w;
-			mHeight = textSurface->h;
-		}
-
-		SDL_FreeSurface(textSurface);
-	}
-	else
-	{
-		printf("Unable to render text surface! SDL_ttf Error: %s\n", TTF_GetError());
-	}
-
-	return mTexture != NULL;
-}
-
-void LTexture::render(int x, int y, SDL_Rect* clip, double angle, SDL_Point* center, SDL_RendererFlip flip)
-{
-	//Set rendering space and render to screen
-	SDL_Rect renderQuad = { x, y, mWidth, mHeight };
-
-	//Set clip rendering dimensions
-	if (clip != NULL)
-	{
-		renderQuad.w = clip->w;
-		renderQuad.h = clip->h;
-	}
-
-	//Render to screen
-	SDL_RenderCopyEx(renderer, mTexture, clip, &renderQuad, angle, center, flip);
-}
-
-class LTimer
-{
-public:
-	//Initializes variables
-	LTimer();
-
-	//The various clock actions
-	void start();
-
-	//Gets the timer's time
-	Uint32 getTicks();
-
-	//Checks the status of the timer
-	bool isStarted();
-	bool isPaused();
-
-private:
-	//The clock time when the timer started
-	Uint32 mStartTicks;
-};
-
-LTimer::LTimer()
-{
-	//Initialize the variables
-	mStartTicks = 0;
-}
-
-void LTimer::start()
-{
-	mStartTicks = SDL_GetTicks();
-}
-
-Uint32 LTimer::getTicks()
-{
-	Uint32 time = 0;
-
-	time = SDL_GetTicks() - mStartTicks;
-
-	return time;
-}
-
-bool init()
-{
-	bool success = true;
-
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
-	{
-		printf("SDL could not initialize! SDL Error %s\n", SDL_GetError());
-		success = false;
-	}
-	else
-	{
-		if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1"))
-		{
-			printf("Warning: Linear texture filtering not enabled!");
-		}
-
-		//Create Window
-		window = SDL_CreateWindow("FPS Cap Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (window == NULL)
-		{
-			printf("Window could not be created! SDL Error %s\n", SDL_GetError());
-			success = false;
-		}
-		else
-		{
-			renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-			if (renderer == NULL)
-			{
-				printf("Renderer could not be created! SDL Error %s\n", SDL_GetError());
-				success = false;
-			}
-			else
-			{
-				SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-
-				if (TTF_Init() == -1)
-				{
-					printf("SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError());
-					success = false;
-				}
-			}
-		}
-	}
-	return success;
-}
-
-bool loadMedia()
-{
-	bool success = true;
-
-	fontFace = TTF_OpenFont("25_capping_frame_rate/lazy.ttf", 28);
-	if(fontFace == NULL)
-	{
-		printf("Failed to load lazy font! SDL_ttf Error: %s\n", TTF_GetError());
-		success = false;
-	}
-
-	return success;
-}
-
-void close()
-{
-	gFPSTextTexture.free();
-
-	TTF_CloseFont(fontFace);
-	fontFace = NULL;
-
-	SDL_DestroyRenderer(renderer);
-	renderer = NULL;
-	SDL_DestroyWindow(window);
-	window = NULL;
-
-	TTF_Quit();
-	SDL_Quit();
-}
-
-int main(int argc, char* args[])
-{
-	if (!init())
-	{
-		printf("Failed to initialize!\n");
-	}
-	else
-	{
-		if (!loadMedia())
-		{
-			printf("Failed to load media!\n");
-		}
-		else
-		{
-			bool quit = false;
-			SDL_Event e;
-			SDL_Color textColor = { 0,0,0,255 };
-			LTimer fpsTimer;
-			LTimer capTimer;
-			std::stringstream timeText;
-			int countedFrames = 0;
-			fpsTimer.start();
-
-			while (!quit)
-			{
-				capTimer.start();
-				while (SDL_PollEvent(&e) != 0)
-				{
-					if (e.type == SDL_QUIT)
-					{
-						quit = true;
-					}
-				}
-
-				float avgFPS = countedFrames / (fpsTimer.getTicks() / 1000.f);
-				if (avgFPS > 2000)
-				{
-					avgFPS = 0;
-				}
-
-				timeText.str("");
-				timeText << "Average FPS(with cap): " << avgFPS;
-
-				if(!gFPSTextTexture.loadFromRenderedText(timeText.str().c_str(), textColor))
-				{
-					printf("Unable to render FPS texture!\n");
-				}
-
-				SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
-				SDL_RenderClear(renderer);
-
-				gFPSTextTexture.render(0, 0);
-				SDL_RenderPresent(renderer);
-
-				++countedFrames;
-
-				int frameTicks = capTimer.getTicks();
-				if (frameTicks < SCREEN_TICK_PER_FRAME)
-				{
-					//Wait remaining time
-					SDL_Delay(SCREEN_TICK_PER_FRAME - frameTicks);
-				}
-			}
-		}
-	}
-	close();
-	return 0;
-}
+//// Manually Capping Screen Time
+//
+//#include <SDL.h>
+//#include <stdio.h>
+//#include <string>
+//#include <SDL_ttf.h>
+//#include <sstream>
+//
+//const int SCREEN_WIDTH = 640;
+//const int SCREEN_HEIGHT = 480;
+//const int SCREEN_FPS = 60;
+//const int SCREEN_TICK_PER_FRAME = 1000 / SCREEN_FPS;
+//
+//class LTexture
+//{
+//public:
+//	//Initializes variables
+//	LTexture();
+//
+//	//Deallocates memory
+//	~LTexture();
+//
+//	//Creates image from font string
+//	bool loadFromRenderedText(std::string textureText, SDL_Color textColor);
+//
+//	//Deallocates texture
+//	void free();
+//
+//	void render(int x, int y, SDL_Rect* clip = NULL, double angle = 0, SDL_Point* center = NULL, SDL_RendererFlip flip = SDL_FLIP_NONE);
+//
+//private:
+//	//The actual hardware texture
+//	SDL_Texture* mTexture;
+//	int mWidth;
+//	int mHeight;
+//};
+//
+//LTexture::LTexture()
+//{
+//	//Initialize
+//	mTexture = NULL;
+//	mWidth = 0;
+//	mHeight = 0;
+//}
+//
+//LTexture::~LTexture()
+//{
+//	//Deallocate
+//	free();
+//}
+//
+//void LTexture::free()
+//{
+//	if (mTexture != NULL)
+//	{
+//		SDL_DestroyTexture(mTexture);
+//		mTexture = NULL;
+//		mWidth = 0;
+//		mHeight = 0;
+//	}
+//}
+//
+//SDL_Window* window = NULL;
+//SDL_Renderer* renderer = NULL;
+//
+//TTF_Font* fontFace = NULL;
+//LTexture gFPSTextTexture;
+//
+//bool LTexture::loadFromRenderedText(std::string textureText, SDL_Color textColor)
+//{
+//	free();
+//
+//	SDL_Surface* textSurface = TTF_RenderText_Solid(fontFace, textureText.c_str(), textColor);
+//	if (textSurface != NULL)
+//	{
+//		mTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
+//		if (mTexture == NULL)
+//		{
+//			printf("Unable to create texture from rendered text! SDL Error %s\n", SDL_GetError());
+//		}
+//		else
+//		{
+//			mWidth = textSurface->w;
+//			mHeight = textSurface->h;
+//		}
+//
+//		SDL_FreeSurface(textSurface);
+//	}
+//	else
+//	{
+//		printf("Unable to render text surface! SDL_ttf Error: %s\n", TTF_GetError());
+//	}
+//
+//	return mTexture != NULL;
+//}
+//
+//void LTexture::render(int x, int y, SDL_Rect* clip, double angle, SDL_Point* center, SDL_RendererFlip flip)
+//{
+//	//Set rendering space and render to screen
+//	SDL_Rect renderQuad = { x, y, mWidth, mHeight };
+//
+//	//Set clip rendering dimensions
+//	if (clip != NULL)
+//	{
+//		renderQuad.w = clip->w;
+//		renderQuad.h = clip->h;
+//	}
+//
+//	//Render to screen
+//	SDL_RenderCopyEx(renderer, mTexture, clip, &renderQuad, angle, center, flip);
+//}
+//
+//class LTimer
+//{
+//public:
+//	//Initializes variables
+//	LTimer();
+//
+//	//The various clock actions
+//	void start();
+//
+//	//Gets the timer's time
+//	Uint32 getTicks();
+//
+//	//Checks the status of the timer
+//	bool isStarted();
+//	bool isPaused();
+//
+//private:
+//	//The clock time when the timer started
+//	Uint32 mStartTicks;
+//};
+//
+//LTimer::LTimer()
+//{
+//	//Initialize the variables
+//	mStartTicks = 0;
+//}
+//
+//void LTimer::start()
+//{
+//	mStartTicks = SDL_GetTicks();
+//}
+//
+//Uint32 LTimer::getTicks()
+//{
+//	Uint32 time = 0;
+//
+//	time = SDL_GetTicks() - mStartTicks;
+//
+//	return time;
+//}
+//
+//bool init()
+//{
+//	bool success = true;
+//
+//	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+//	{
+//		printf("SDL could not initialize! SDL Error %s\n", SDL_GetError());
+//		success = false;
+//	}
+//	else
+//	{
+//		if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1"))
+//		{
+//			printf("Warning: Linear texture filtering not enabled!");
+//		}
+//
+//		//Create Window
+//		window = SDL_CreateWindow("FPS Cap Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+//		if (window == NULL)
+//		{
+//			printf("Window could not be created! SDL Error %s\n", SDL_GetError());
+//			success = false;
+//		}
+//		else
+//		{
+//			renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+//			if (renderer == NULL)
+//			{
+//				printf("Renderer could not be created! SDL Error %s\n", SDL_GetError());
+//				success = false;
+//			}
+//			else
+//			{
+//				SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+//
+//				if (TTF_Init() == -1)
+//				{
+//					printf("SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError());
+//					success = false;
+//				}
+//			}
+//		}
+//	}
+//	return success;
+//}
+//
+//bool loadMedia()
+//{
+//	bool success = true;
+//
+//	fontFace = TTF_OpenFont("25_capping_frame_rate/lazy.ttf", 28);
+//	if(fontFace == NULL)
+//	{
+//		printf("Failed to load lazy font! SDL_ttf Error: %s\n", TTF_GetError());
+//		success = false;
+//	}
+//
+//	return success;
+//}
+//
+//void close()
+//{
+//	gFPSTextTexture.free();
+//
+//	TTF_CloseFont(fontFace);
+//	fontFace = NULL;
+//
+//	SDL_DestroyRenderer(renderer);
+//	renderer = NULL;
+//	SDL_DestroyWindow(window);
+//	window = NULL;
+//
+//	TTF_Quit();
+//	SDL_Quit();
+//}
+//
+//int main(int argc, char* args[])
+//{
+//	if (!init())
+//	{
+//		printf("Failed to initialize!\n");
+//	}
+//	else
+//	{
+//		if (!loadMedia())
+//		{
+//			printf("Failed to load media!\n");
+//		}
+//		else
+//		{
+//			bool quit = false;
+//			SDL_Event e;
+//			SDL_Color textColor = { 0,0,0,255 };
+//			LTimer fpsTimer;
+//			LTimer capTimer;
+//			std::stringstream timeText;
+//			int countedFrames = 0;
+//			fpsTimer.start();
+//
+//			while (!quit)
+//			{
+//				capTimer.start();
+//				while (SDL_PollEvent(&e) != 0)
+//				{
+//					if (e.type == SDL_QUIT)
+//					{
+//						quit = true;
+//					}
+//				}
+//
+//				float avgFPS = countedFrames / (fpsTimer.getTicks() / 1000.f);
+//				if (avgFPS > 2000)
+//				{
+//					avgFPS = 0;
+//				}
+//
+//				timeText.str("");
+//				timeText << "Average FPS(with cap): " << avgFPS;
+//
+//				if(!gFPSTextTexture.loadFromRenderedText(timeText.str().c_str(), textColor))
+//				{
+//					printf("Unable to render FPS texture!\n");
+//				}
+//
+//				SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+//				SDL_RenderClear(renderer);
+//
+//				gFPSTextTexture.render(0, 0);
+//				SDL_RenderPresent(renderer);
+//
+//				++countedFrames;
+//
+//				int frameTicks = capTimer.getTicks();
+//				if (frameTicks < SCREEN_TICK_PER_FRAME)
+//				{
+//					//Wait remaining time
+//					SDL_Delay(SCREEN_TICK_PER_FRAME - frameTicks);
+//				}
+//			}
+//		}
+//	}
+//	close();
+//	return 0;
+//}
