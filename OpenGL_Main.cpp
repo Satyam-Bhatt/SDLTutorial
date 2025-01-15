0dec61cb6eefbdfc0280ceeee3675ba7f7a24fea
Satyam-Bhatt <satyambhatt20@gmail.com>
2025-01-15 12:32:40 +0530

    OpenGL working finally

--- OpenGL_Main.cpp
+++ OpenGL_Main.cpp
@@ -9,7 +9,7 @@
 #include "DataStream.h"
 #include <SDL_thread.h>
 #include <SDL_opengl.h>
-#include <GL\GLU.h>
+//#include <GL\GLU.h>
 #define SDL_TTF_MAJOR_VERSION
 
 //Starts up SDL, creates window, and initializes OpenGL
@@ -37,7 +37,7 @@ SDL_Window* gWindow = NULL;
 SDL_GLContext gContext;
 
 //Render flag
-bool gRenderQUad = true;
+bool gRenderQuad = true;
 
 bool init()
 {
@@ -65,24 +65,165 @@ bool init()
 		}
 		else
 		{
-			//Use Vsync
-			if(SDL_GL_SetSwapInterval(1) < 0)
+			//Create context
+			gContext = SDL_GL_CreateContext(gWindow);
+			if (gContext == NULL)
 			{
-				printf("Warning: Unable to set VSync! SDL Error: %s\n", SDL_GetError());
-			}
-
-			//Initialize OpenGL
-			if(!initGL())
-			{
-				printf("Failed to initialize OpenGL!\n");
+				printf("OpenGL context could not be created! SDL Error: %s\n", SDL_GetError());
 				success = false;
+			}
+			else
+			{
+				//Use Vsync
+				if(SDL_GL_SetSwapInterval(1) < 0)
+				{
+					printf("Warning: Unable to set VSync! SDL Error: %s\n", SDL_GetError());
+				}
+
+				//Initialize OpenGL
+				if(!initGL())
+				{
+					printf("Failed to initialize OpenGL!\n");
+					success = false;
+				}
 			}
 		}
 	}
+
 	return success;
+}
+
+bool initGL()
+{
+	bool success = true;
+	GLenum error = GL_NO_ERROR;
+
+	//Initialize Projection Matrix
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	//Check for error
+	error = glGetError();
+	if(error != GL_NO_ERROR)
+	{
+		//printf("Error initializing OpenGL! %s\n", gluErrorString(error));
+		success = false;
+	}
+
+	//Initialize Modelview Matrix
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	//Check for error
+	error = glGetError();
+	if(error != GL_NO_ERROR)
+	{
+		//printf("Error initializing OpenGL! %s\n", gluErrorString(error));
+		success = false;
+	}
+
+	//Initialize clear color
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+	//Check for error
+	error = glGetError();
+	if(error != GL_NO_ERROR)
+	{
+		//printf("Error initializing OpenGL! %s\n", gluErrorString(error));
+		success = false;
+	}
+
+	return success;
+}
+
+void handleKeys(unsigned char key, int x, int y) 
+{
+	//Toggle quad
+	if(key == 'q')
+	{
+		gRenderQuad = !gRenderQuad;
+	}
+}
+
+void update()
+{
+	//No per frame update needed
+}
+
+void render()
+{
+	//Clear color buffer
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	//Render quad
+	if (gRenderQuad)
+	{
+		glBegin(GL_QUADS);
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f(0.5f, -0.5f);
+		glVertex2f(0.5f, 0.5f);
+		glVertex2f(-0.5f, 0.5f);
+		glEnd();
+	}
+}
+
+void close()
+{
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+	SDL_Quit();
 }
 
 int main(int argc, char* args[])
 {
+	//Starts up SDL and create window
+	if(!init())
+	{
+		printf("Failed to initialize!\n");
+	}
+	else
+	{
+		//Main loop flag
+		bool quit = false;
+
+		//Event handler
+		SDL_Event e;
+	
+		//Enable text input
+		SDL_StartTextInput();
+
+		//While application is running
+		while (!quit)
+		{
+			//Handle events on queue
+			while (SDL_PollEvent(&e) != 0)
+			{
+				//User requests quit
+				if (e.type == SDL_QUIT)
+				{
+					quit = true;
+				}
+				//Handle key presses with current mouse position
+				else if (e.type == SDL_TEXTINPUT)
+				{
+					int x = 0, y = 0;
+					SDL_GetMouseState(&x, &y);
+					handleKeys(e.text.text[0], x, y);
+				}
+			}
+
+			//Render Quad
+			render();
+
+			//Update screen
+			SDL_GL_SwapWindow(gWindow);
+		}
+
+		//Disable text input
+		SDL_StopTextInput();
+	}
+	//Free resources and close SDL
+	close();
+
 	return 0;
 }
